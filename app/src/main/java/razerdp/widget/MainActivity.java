2140815164be4e05817c420597885173f7906b1e
430public <164701463@qq.com>
2016-01-21 17:34:14 +0800

    // first commit

---
+++ app/src/main/java/razerdp/widget/MainActivity.java
@@ -0,0 +1,54 @@
+package razerdp.widget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button addButton;
+    private Button subButton;
+    private UnderLineLinearLayout mUnderLineLinearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addButton = (Button) findViewById(R.id.add);
+        subButton = (Button) findViewById(R.id.sub);
+        mUnderLineLinearLayout = (UnderLineLinearLayout) findViewById(R.id.underline_layout);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+        subButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subItem();
+            }
+        });
+    }
+
+    int i = 0;
+
+    private void addItem() {
+        View v = LayoutInflater.from(this).inflate(R.layout.item_test, mUnderLineLinearLayout, false);
+        ((TextView) v.findViewById(R.id.tx_action)).setText("this is test " + i);
+        ((TextView) v.findViewById(R.id.tx_action_time)).setText("2016-01-21");
+        ((TextView) v.findViewById(R.id.tx_action_status)).setText("finish");
+        mUnderLineLinearLayout.addView(v);
+        i++;
+    }
+
+    private void subItem() {
+        if (mUnderLineLinearLayout.getChildCount() > 0) {
+            mUnderLineLinearLayout.removeViews(mUnderLineLinearLayout.getChildCount() - 1, 1);
+            i--;
+        }
+    }
+}
---
+++ library/src/main/java/razerdp/widget/UnderLineLinearLayout.java
@@ -0,0 +1,166 @@
+package razerdp.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import razerdp.underlinelinearlayout.R;
+
+/**
+ * Created by 大灯泡 on 2016/1/21.
+ * 简易带有时间轴的linearlayout
+ */
+public class UnderLineLinearLayout extends LinearLayout {
+    //=============================================================元素定义
+    private Bitmap mIcon;
+    //line location
+    private int lineMarginSide;
+    private int lineDynamicDimen;
+    //line property
+    private int lineStrokeWidth;
+    private int lineColor;
+    //point property
+    private int pointSize;
+    private int pointColor;
+
+    //=============================================================paint
+    private Paint linePaint;
+    private Paint pointPaint;
+    //=============================================================其他辅助参数
+    //第一个点的位置
+    private int firstX;
+    private int firstY;
+    //最后一个图的位置
+    private int lastX;
+    private int lastY;
+    //默认垂直
+    private int curOrientation = VERTICAL;
+    private Context mContext;
+
+    //开关
+    private boolean drawLine = true;
+
+    public UnderLineLinearLayout(Context context) {
+        this(context, null);
+    }
+
+    public UnderLineLinearLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public UnderLineLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray attr = context.obtainStyledAttributes(attrs, R.styleable.UnderLineLinearLayout);
+        lineMarginSide = attr.getDimensionPixelOffset(R.styleable.UnderLineLinearLayout_line_margin_side, 10);
+        lineDynamicDimen = attr.getDimensionPixelOffset(R.styleable.UnderLineLinearLayout_line_dynamic_dimen, 0);
+        lineStrokeWidth = attr.getDimensionPixelOffset(R.styleable.UnderLineLinearLayout_line_stroke_width, 2);
+        lineColor = attr.getColor(R.styleable.UnderLineLinearLayout_line_color, 0xff3dd1a5);
+        pointSize = attr.getDimensionPixelSize(R.styleable.UnderLineLinearLayout_point_size, 8);
+        pointColor = attr.getDimensionPixelOffset(R.styleable.UnderLineLinearLayout_point_color, 0xff3dd1a5);
+
+        int iconRes = attr.getResourceId(R.styleable.UnderLineLinearLayout_icon_src, R.drawable.ic_ok);
+        BitmapDrawable temp = (BitmapDrawable) context.getResources().getDrawable(iconRes);
+        if (temp != null) mIcon = temp.getBitmap();
+
+        curOrientation = getOrientation();
+
+        initView(context);
+    }
+
+    private void initView(Context context) {
+        this.mContext = context;
+
+        linePaint = new Paint();
+        linePaint.setAntiAlias(true);
+        linePaint.setDither(true);
+        linePaint.setColor(lineColor);
+        linePaint.setStrokeWidth(lineStrokeWidth);
+        linePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        pointPaint = new Paint();
+        pointPaint.setAntiAlias(true);
+        pointPaint.setDither(true);
+        pointPaint.setColor(pointColor);
+        pointPaint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (drawLine) {
+            drawTimeLine(canvas);
+        }
+    }
+
+    private void drawTimeLine(Canvas canvas) {
+        int childCount = getChildCount();
+
+        if (childCount > 0) {
+            //大于1，证明至少有2个，也就是第一个和第二个之间连成线，第一个和最后一个分别有点/icon
+            if (childCount > 1) {
+                switch (curOrientation) {
+                    case VERTICAL:
+                        drawFirstChildViewVertical(canvas);
+                        drawLastChildViewVertical(canvas);
+                        drawBetweenLineVertical(canvas);
+                        break;
+                    case HORIZONTAL:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (childCount == 1) {
+                switch (curOrientation) {
+                    case VERTICAL:
+                        drawFirstChildViewVertical(canvas);
+                        break;
+                    case HORIZONTAL:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void drawFirstChildViewVertical(Canvas canvas) {
+        if (getChildAt(0) != null) {
+            int top = getChildAt(0).getTop();
+            //记录值
+            firstX = lineMarginSide;
+            firstY = top + getChildAt(0).getPaddingTop() + lineDynamicDimen;
+            //画一个圆
+            canvas.drawCircle(firstX, firstY, pointSize, pointPaint);
+        }
+    }
+
+    private void drawLastChildViewVertical(Canvas canvas) {
+        if (getChildAt(getChildCount() - 1) != null) {
+            int top = getChildAt(getChildCount() - 1).getTop();
+            //记录值
+            lastX = lineMarginSide - (mIcon.getWidth() >> 1);
+            lastY = top + getChildAt(getChildCount() - 1).getPaddingTop() + lineDynamicDimen;
+            //画一个图
+            canvas.drawBitmap(mIcon, lastX, lastY, null);
+        }
+    }
+
+    private void drawBetweenLineVertical(Canvas canvas) {
+        for (int i = 0; i < getChildCount() - 1; i++) {
+            //画剩下的
+            canvas.drawLine(lineMarginSide, firstY, lineMarginSide, lastY, linePaint);
+            //画了线，就画圆
+            if (getChildAt(i) != null && i != 0) {
+                int top = getChildAt(i).getTop();
+                //记录值
+                int Y = top + getChildAt(i).getPaddingTop() + lineDynamicDimen;
+                canvas.drawCircle(lineMarginSide, Y, pointSize, pointPaint);
+            }
+        }
+    }
+}
